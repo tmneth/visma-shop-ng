3d150508cacfdc805f7a22f91a99d50655a150f2
tmneth <timeneth@gmail.com>
2023-09-12 10:48:07 +0300

    Create header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ShopComponent } from './shop/shop.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [ShopComponent],
+  imports: [HeaderComponent, ShopComponent],
 })
 export class AppComponent {
   title = 'visma-shop-ng';
