af8ee0e20fd631da0671d8eb193bbaf1b4b60993
tmneth <timeneth@gmail.com>
2023-09-12 10:15:32 +0300

    Add a Pipe to truncate long strings

--- src/app/shop/product/product.component.ts
+++ src/app/shop/product/product.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { TruncateString } from 'src/app/shared/pipes/truncate-string.pipe';
 
 @Component({
   standalone: true,
   selector: 'app-product',
   templateUrl: './product.component.html',
+  imports: [TruncateString],
 })
 export class ProductComponent {
   @Input() product: any;
