5c4084dec243ce6876ea5f270cbcba79eb0e061c
tmneth <80415416+tmneth@users.noreply.github.com>
2023-09-13 14:07:40 +0300

    Only import necessary RouterModule directives

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-header',
   templateUrl: './header.component.html',
-  imports: [RouterModule],
+  imports: [RouterLink, RouterLinkActive],
 })
 export class HeaderComponent {}
