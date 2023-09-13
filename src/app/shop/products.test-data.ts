5469c7f2a04df098f8845eb0ab778bc27164ad71
tmneth <timeneth@gmail.com>
2023-09-13 12:26:36 +0300

    Add test data

---
+++ src/app/shop/products.test-data.ts
@@ -0,0 +1,38 @@
+export const productsData = [
+  {
+    id: 1,
+    name: 'Cool black hoodie',
+    description: 'Some description goes here',
+    price: 15,
+    discount: 3,
+    imagePath:
+      'https://images.unsplash.com/photo-1556821840-3a63f95609a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG9vZGllfGVufDB8fDB8fHww&w=1000&q=80',
+  },
+  {
+    id: 2,
+    name: 'Sports hoodie',
+    description: 'Some description goes here',
+    price: 18,
+    discount: null,
+    imagePath:
+      'https://lovet.g.shopcadacdn.com/sites/files/lovet/images/products/202305/1000x1500/21may-r1-235.jpg',
+  },
+  {
+    id: 3,
+    name: 'Cool black designer hoodie',
+    description: 'Some description goes here',
+    price: 35,
+    discount: null,
+    imagePath:
+      'https://images.unsplash.com/photo-1615397587950-3cbb55f95b77?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGhvb2RpZXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80',
+  },
+  {
+    id: 4,
+    name: 'Grey hoodie',
+    description: 'Some description goes here',
+    price: 20,
+    discount: null,
+    imagePath:
+      'https://img.freepik.com/free-photo/beautiful-woman-white-hoodie-winter-fashion_53876-119758.jpg',
+  },
+];
