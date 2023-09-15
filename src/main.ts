815bd5455f37daadbe4a0c21ad6a760cf4595ef7
tmneth <timeneth@gmail.com>
2023-09-15 13:17:08 +0300

    Set up client/server folder structure

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-import { APP_ROUTES } from './app.routes';
-
-bootstrapApplication(AppComponent, {
-  providers: [provideAnimations(), provideRouter(APP_ROUTES)],
-});
