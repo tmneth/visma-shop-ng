26874a46aa9f34b488645dc16596bdc4a0e1c449
tmneth <80415416+tmneth@users.noreply.github.com>
2023-09-15 06:50:45 +0300

    Add ProductForm model

--- src/app/shared/models/product.model.ts
+++ src/app/shared/models/product.model.ts
@@ -1,8 +1,19 @@
+import { FormControl } from '@angular/forms';
+
 export interface Product {
   id: number;
   name: string;
   description: string;
   price: number;
-  discount?: number | null;
+  discount: number | null;
   imagePath: string;
+}
+
+export interface ProductForm {
+  id: FormControl<number | null>;
+  name: FormControl<string | null>;
+  description: FormControl<string | null>;
+  price: FormControl<number | null>;
+  discount: FormControl<number | null>;
+  imagePath: FormControl<string | null>;
 }
