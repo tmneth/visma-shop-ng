1634b24c5a48d66f9335ef72be98a60ee6e13fc3
tmneth <80415416+tmneth@users.noreply.github.com>
2023-09-19 08:21:13 +0300

    Set up HttpClient module

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { APP_ROUTES } from './app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideAnimations(), provideRouter(APP_ROUTES)],
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),
+    provideRouter(APP_ROUTES),
+  ],
 });
