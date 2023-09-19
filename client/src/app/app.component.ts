0fdb330c02cb54fdb6bb15aec0d6ffaf57b26ff8
tmneth <80415416+tmneth@users.noreply.github.com>
2023-09-19 08:54:26 +0300

    Clean up app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -7,9 +7,6 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
   imports: [HeaderComponent, ShopComponent, RouterModule],
 })
-export class AppComponent {
-  title = 'visma-shop-ng';
-}
+export class AppComponent {}
