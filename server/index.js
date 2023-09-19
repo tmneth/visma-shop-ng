e35f7b5f67cafb0a0ff38014ec4e8eeac06b6d5f
tmneth <80415416+tmneth@users.noreply.github.com>
2023-09-19 07:51:27 +0300

    Set up express server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import productsRoutes from "./routes/products.js";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use("/products", productsRoutes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
