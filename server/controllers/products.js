06e133cc0f8aa31aaa56ba53443532a5e07d9d14
tmneth <80415416+tmneth@users.noreply.github.com>
2023-09-19 07:51:51 +0300

    Implement controller to GET all products

---
+++ server/controllers/products.js
@@ -0,0 +1,17 @@
+import { getJsonData, writeJsonData } from "../utils/helpers.js";
+
+export const getProducts = async (req, res) => {
+  try {
+    const jsonData = await getJsonData();
+    res.status(200).json(jsonData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to read the products data" });
+  }
+};
+
+export const createProduct = async (req, res) => {};
+
+export const updateProduct = async (req, res) => {};
+
+export const deleteProduct = async (req, res) => {};
