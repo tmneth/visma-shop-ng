17ab66ea8a32e107acd123f564fb970bb5398719
tmneth <80415416+tmneth@users.noreply.github.com>
2023-09-20 07:08:39 +0300

    Add controller to create a new product

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -2,6 +2,8 @@ import { nanoid } from "nanoid/non-secure";
 import { getJsonData, writeJsonData } from "../utils/helpers.js";
 
 export const getProducts = async (req, res) => {
+  console.log(nanoid(5), nanoid(5), nanoid(5), nanoid(5), nanoid(5));
+
   try {
     const jsonData = await getJsonData();
     res.status(200).json(jsonData);
@@ -11,7 +13,33 @@ export const getProducts = async (req, res) => {
   }
 };
 
-export const createProduct = async (req, res) => {};
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, discount, imageUrl } = req.body;
+
+    const jsonData = await getJsonData();
+
+    const newProduct = {
+      id: nanoid(5),
+      name,
+      description,
+      price,
+      discount,
+      imageUrl,
+    };
+
+    jsonData.products.push(newProduct);
+
+    await writeJsonData(jsonData);
+
+    res
+      .status(201)
+      .json({ message: "Product created successfully", product: newProduct });
+  } catch (error) {
+    console.error("Error creating product:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 
 export const updateProduct = async (req, res) => {};
 
