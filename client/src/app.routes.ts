e5491fb86bb77cd8b902458a3facdc1a389ab805
tmneth <80415416+tmneth@users.noreply.github.com>
2023-09-21 08:08:25 +0300

    Rename form component

--- client/src/app.routes.ts
+++ client/src/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { ShopComponent } from './app/pages/shop/shop.component';
-import { FormComponent } from './app/pages/form/form.component';
+import { ProductFormComponent } from './app/pages/product-form/product-form.component';
 
 export const APP_ROUTES: Routes = [
   { path: 'shop', component: ShopComponent },
   { path: 'cart', component: ShopComponent },
-  { path: 'form', component: FormComponent },
+  { path: 'form', component: ProductFormComponent },
   { path: '**', redirectTo: 'shop' },
 ];
---
+++ client/src/app/pages/product-form/product-form.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+  NonNullableFormBuilder,
+} from '@angular/forms';
+import { Product } from 'src/app/shared/data-services/models/product.view.model';
+
+import { ProductComponent } from '../shop/product/product.component';
+import { CommonModule } from '@angular/common';
+import { urlValidator } from '../../shared/validators/url.validator';
+import { ShopService } from '../../shared/data-services/services/shop.data.service';
+
+@Component({
+  standalone: true,
+  selector: 'app-product-form',
+  templateUrl: './product-form.component.html',
+  imports: [ReactiveFormsModule, ProductComponent, FormsModule, CommonModule],
+})
+export class ProductFormComponent {
+  constructor(private shop: ShopService, private fb: NonNullableFormBuilder) {}
+
+  private _discountFieldIsDisplayed = false;
+
+  get discountFieldIsDisplayed(): boolean {
+    return this._discountFieldIsDisplayed;
+  }
+
+  set discountFieldIsDisplayed(value: boolean) {
+    this._discountFieldIsDisplayed = value;
+    this.adjustDiscountField();
+  }
+
+  adjustDiscountField() {
+    const discountControl = this.productForm.get('discount');
+    if (this._discountFieldIsDisplayed) {
+      discountControl?.setValidators([
+        Validators.required,
+        Validators.max(500),
+      ]);
+    } else {
+      discountControl?.setValue(0);
+      discountControl?.setValidators(null);
+    }
+    discountControl?.updateValueAndValidity();
+  }
+
+  currentPrice: number = 0;
+
+  productForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(5)]],
+    description: [''],
+    price: [0, [Validators.required, Validators.max(500)]],
+    discount: [0, [Validators.required, Validators.max(500)]],
+    imageUrl: ['', [Validators.required, urlValidator()]],
+  });
+
+  get productFormControl() {
+    return this.productForm.controls;
+  }
+
+  onSubmit() {
+    console.log(this.productForm.value);
+
+    this.shop.createProduct(this.productForm.value as Product).subscribe({
+      next: (data) => console.log(data),
+      error: (err: Error) => console.error('Observer got an error: ' + err),
+    });
+  }
+}
