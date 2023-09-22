bc8b112c2194c07c1d71e7a8806e47f921947f34
tmneth <timeneth@gmail.com>
2023-09-22 11:23:25 +0300

    Change route name for product addition

--- client/src/app.routes.ts
+++ client/src/app.routes.ts
@@ -5,7 +5,7 @@ import { ProductFormComponent } from './app/pages/product-form/product-form.comp
 export const APP_ROUTES: Routes = [
   { path: 'shop', component: ShopComponent },
   { path: 'cart', component: ShopComponent },
-  { path: 'form', component: ProductFormComponent },
+  { path: 'add-product', component: ProductFormComponent },
   { path: 'edit-product/:id', component: ProductFormComponent },
   { path: '**', redirectTo: 'shop' },
 ];
