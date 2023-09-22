e131510f9f47b5bb3d0018f1a674c4bd573d7ae4
tmneth <timeneth@gmail.com>
2023-09-22 15:18:03 +0300

    Add work around to convert postgres strings back to numbers

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,11 @@ import pg from "pg";
 const { Pool } = pg;
 dotenv.config();
 
+const { types } = pg;
+
+// Parser number 1700 is the for floating point types. Here we’re overriding it to use the vanilla JavaScript parseFloat() function
+types.setTypeParser(1700, (x) => parseFloat(x));
+
 const pool = new Pool({
   user: process.env.PGUSER,
   host: process.env.PGHOST,
