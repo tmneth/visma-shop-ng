fd411730898726c6c806525462bd184205e6dc19
tmneth <timeneth@gmail.com>
2023-09-22 13:54:41 +0300

    Replace fs with psql queries

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,12 +1,11 @@
 import { nanoid } from "nanoid/non-secure";
-import { getJsonData, writeJsonData } from "../utils/helpers.js";
+import * as db from "../db/queries.js";
 
 export const getProducts = async (req, res) => {
-  console.log(nanoid(5), nanoid(5), nanoid(5), nanoid(5), nanoid(5));
-
   try {
-    const jsonData = await getJsonData();
-    res.status(200).json(jsonData);
+    const products = await db.getProducts();
+    console.log(products);
+    res.status(200).json(products);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Failed to read the products data" });
@@ -16,9 +15,7 @@ export const getProducts = async (req, res) => {
 export const getProduct = async (req, res) => {
   const id = req.params.id;
   try {
-    const jsonData = await getJsonData();
-    const product = jsonData.products.find((p) => p.id === id);
-
+    const product = await db.getProductById(id);
     if (product) {
       res.status(200).json(product);
     } else {
@@ -26,77 +23,56 @@ export const getProduct = async (req, res) => {
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: "Failed to read the products data" });
+    res.status(500).json({ error: "Failed to retrieve the product" });
   }
 };
 
 export const createProduct = async (req, res) => {
+  const { name, description, price, discount, imageUrl } = req.body;
+  const id = nanoid(5);
   try {
-    const { name, description, price, discount, imageUrl } = req.body;
-
-    const jsonData = await getJsonData();
-
-    const newProduct = {
-      id: nanoid(5),
+    const newProduct = await db.addProduct({
+      id,
       name,
       description,
       price,
       discount,
       imageUrl,
-    };
-
-    jsonData.products.push(newProduct);
-
-    await writeJsonData(jsonData);
-
-    res
-      .status(201)
-      .json({ message: "Product created successfully", product: newProduct });
-  } catch (error) {
-    console.error("Error creating product:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+    });
+    res.status(201).json(newProduct);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to add the product" });
   }
 };
 
 export const updateProduct = async (req, res) => {
   const id = req.params.id;
   const { name, description, price, discount, imageUrl } = req.body;
-
   try {
-    const jsonData = await getJsonData();
-    const product = jsonData.products.find((p) => p.id === id);
-
-    if (product) {
-      if (name) product.name = name;
-      if (description) product.description = description;
-      if (price) product.price = price;
-      if (discount) product.discount = discount;
-      if (imageUrl) product.imageUrl = imageUrl;
-
-      await writeJsonData(jsonData);
-
-      res
-        .status(200)
-        .json({ message: "Product updated successfully", product });
+    const updatedProduct = await db.updateProduct(id, {
+      name,
+      description,
+      price,
+      discount,
+      imageUrl,
+    });
+    if (updatedProduct) {
+      res.status(200).json(updatedProduct);
     } else {
       res.status(404).json({ message: "Product not found!" });
     }
   } catch (err) {
-    console.error("Error updating product:", err);
-    res.status(500).json({ error: "Internal Server Error" });
+    console.error(err);
+    res.status(500).json({ error: "Failed to update the product" });
   }
 };
 
 export const deleteProduct = async (req, res) => {
   const id = req.params.id;
-
   try {
-    const jsonData = await getJsonData();
-    const index = jsonData.products.findIndex((p) => p.id === id);
-
-    if (index !== -1) {
-      jsonData.products.splice(index, 1);
-      writeJsonData(jsonData);
+    const success = await db.deleteProductById(id);
+    if (success) {
       res.status(200).json({ message: "Product deleted successfully!" });
     } else {
       res.status(404).json({ message: "Product not found!" });
---
+++ server/db/queries.js
@@ -0,0 +1,50 @@
+import pool from "./db.js";
+
+export const getProducts = async () => {
+  const { rows: products } = await pool.query("SELECT * FROM products", []);
+  return products;
+};
+
+export const getProductById = async (productId) => {
+  const { rows } = await pool.query("SELECT * FROM products WHERE id = $1", [
+    productId,
+  ]);
+  return rows[0];
+};
+
+export const addProduct = async (product) => {
+  const { rows } = await pool.query(
+    "INSERT INTO products (id, name, description, price, discount, imageUrl) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+    [
+      product.id,
+      product.name,
+      product.description,
+      product.price,
+      product.discount,
+      product.imageUrl,
+    ]
+  );
+  return rows[0];
+};
+
+export const updateProduct = async (productId, product) => {
+  const { rows } = await pool.query(
+    "UPDATE products SET name = $2, description = $3, price = $4, discount = $5, imageUrl = $6 WHERE id = $1 RETURNING *",
+    [
+      productId,
+      product.name,
+      product.description,
+      product.price,
+      product.discount,
+      product.imageUrl,
+    ]
+  );
+  return rows[0];
+};
+
+export const deleteProductById = async (productId) => {
+  const { rowCount } = await pool.query("DELETE FROM products WHERE id = $1", [
+    productId,
+  ]);
+  return rowCount > 0;
+};
