c228c86d9b5ec065179181a0d7c877ff25db1541
tmneth <timeneth@gmail.com>
2023-09-22 13:53:53 +0300

    Connect to postgres db via pg

---
+++ server/db/db.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import pg from "pg";
+const { Pool } = pg;
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+});
+
+export default pool;
