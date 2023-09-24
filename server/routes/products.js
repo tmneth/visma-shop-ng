f703df66806b445643e44e0daab116668b0bb0f1
tmneth <timeneth@gmail.com>
2023-09-24 09:11:14 +0300

    Protect update/delete routes

--- server/routes/products.js
+++ server/routes/products.js
@@ -7,13 +7,14 @@ import {
   updateProduct,
   deleteProduct,
 } from "../controllers/products.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/", getProducts);
 router.post("/", createProduct);
-router.put("/:id", updateProduct);
+router.put("/:id", auth, updateProduct);
 router.get("/:id", getProduct);
-router.delete("/:id", deleteProduct);
+router.delete("/:id", auth, deleteProduct);
 
 export default router;
