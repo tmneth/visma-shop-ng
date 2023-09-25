923c77abc69f0c31e1f3157a93ab1a66c8f8b88a
tmneth <timeneth@gmail.com>
2023-09-25 09:50:24 +0300

    Allow product creation only to authentificated users

--- server/routes/products.js
+++ server/routes/products.js
@@ -12,9 +12,9 @@ import auth from "../middleware/auth.js";
 const router = express.Router();
 
 router.get("/", getProducts);
-router.post("/", createProduct);
-router.put("/:id", auth, updateProduct);
 router.get("/:id", getProduct);
+router.post("/", auth, createProduct);
+router.put("/:id", auth, updateProduct);
 router.delete("/:id", auth, deleteProduct);
 
 export default router;
