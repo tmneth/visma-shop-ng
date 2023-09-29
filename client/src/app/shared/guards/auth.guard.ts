ab234de8a59de9f924baccdbd07a34fc8cd7ec41
tmneth <timeneth@gmail.com>
2023-09-29 08:27:00 +0300

    Remove unused arguments from guard activation function

--- client/src/app/shared/guards/auth.guard.ts
+++ client/src/app/shared/guards/auth.guard.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  Router,
-} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -11,10 +7,7 @@ import {
 export class AuthGuard {
   constructor(private router: Router) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean {
+  canActivate(): boolean {
     const token = localStorage.getItem('token');
     if (!token) {
       this.router.navigate(['/auth']);
